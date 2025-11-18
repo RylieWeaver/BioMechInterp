671bf83fbb4fe9808ec608ffb54828d863fe07f6
Rylie Weaver <rylie@MacBook-Pro-166.local>
2025-11-18 10:56:20 -0500

    Update SAE Model

--- biomechinterp/models/sae.py
+++ biomechinterp/models/sae.py
@@ -17,10 +17,7 @@ from .activation import get_activation
 @dataclass
 class SparseAutoencoderConfig(Config):
     input_dim: int
-    hidden_dim: int
     latent_dim: int
-    activation: str = "relu"
-    dropout: float = 0.0
 
     @staticmethod
     def load(path: Path) -> "SparseAutoencoderConfig":
@@ -33,28 +30,21 @@ class SparseAutoencoder(nn.Module):
     def __init__(self, config: SparseAutoencoderConfig):
         super().__init__()
         self.cfg = config
-        act = get_activation(config.activation)
-        dropout = config.dropout
+        self.act = get_activation("relu")()
         input_dim = config.input_dim
-        hidden_dim = config.hidden_dim
         latent_dim = config.latent_dim
 
         # Define architecture
-        self.encoder = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            act(),
-            nn.Dropout(dropout) if dropout > 0 else nn.Identity(),
-            nn.Linear(hidden_dim, latent_dim)
-        )
-        self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, hidden_dim),
-            act(),
-            nn.Dropout(dropout) if dropout > 0 else nn.Identity(),
-            nn.Linear(hidden_dim, input_dim)
-        )
+        self.encoder = nn.Linear(input_dim, latent_dim)
+        self.decoder = nn.Linear(latent_dim, input_dim)
+
+    def encode(self, x):
+        x = x - self.decoder.bias
+        codes = self.act(self.encoder(x))
+        return codes
 
     def forward(self, inputs):
-        codes = self.encoder(inputs)
+        codes = self.encode(inputs)
         reconstruction = self.decoder(codes)
         return reconstruction, codes
 
